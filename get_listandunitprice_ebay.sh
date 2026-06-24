c9c69f8122b98ec997807546a9fd87eac75dd51d
Your Name <your.email@example.com>
2026-06-24 10:18:53 -0700

    Include get_list,findlow scripts and crontab.

---
+++ get_listandunitprice_ebay.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HOME=/home/ACMECO
+DATE=$(date +%m%d)
+LONGDATE=$(date +%m%d%H%M)
+DOWNLOADS=$HOME/downloads/$DATE
+PRICES=$DOWNLOADS/Prices.txt
+ITEMS=$HOME/ebayitems.txt
+RUN_HOME=$HOME/$DATE
+PRICES_TMP=$RUN_HOME/ListPrices.ebay.tmp.$DATE.csv
+UPDATED_PRICES=$RUN_HOME/ListPrices.ebay.$DATE.csv
+LOGFILE=$RUN_HOME/logs/$(basename "$0").ebay.$LONGDATE.log
+
+{
+echo Begin Retrieve New Price per Item ...
+cat /dev/null > $PRICES_TMP
+cat /dev/null > $UPDATED_PRICES
+
+for item in $(cat $ITEMS);do
+  item+="|"
+  grep $item $PRICES | awk -F'|' '{print $1","$7","$8}' >> $PRICES_TMP
+done 
+
+echo "SKU, LIST_PRICE,UNIT_PRICE" > $UPDATED_PRICES
+cat $PRICES_TMP >> $UPDATED_PRICES
+
+echo End Retrieve New Price per Item
+
+echo End Re-calculate Prices Based on Value 
+
+} |& tee $LOGFILE
